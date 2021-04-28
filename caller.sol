1cb0bbd7a4edcf43132b8804cc75dfa9bc7f7fae
Dimitar Stoyanov <47748614+ZoomMan12312@users.noreply.github.com>
2021-04-28 12:05:16 +0300

    Create caller.sol

---
+++ caller.sol
@@ -0,0 +1,18 @@
+// A basic smart contract that gets the WETH address of an already deployed router contract
+// One is currently deployed on the BSC Testnet on 0xdcD561eEa80210C5ac2ADCE3c173a537d1Baf7FC
+
+//SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.4;
+
+abstract contract Router {
+    function WETH() virtual external pure returns (address);
+}
+
+contract caller {
+    address routerAddress = 0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3;
+    Router routerContract = Router(routerAddress);
+    
+    function getWethAddr() public view returns (address) {
+        return routerContract.WETH();
+    }
+}
